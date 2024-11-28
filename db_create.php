16980d74f0226477f146c2ffc3c722933e1e7602
SkalZ <dima.tysyahnyy@mail.ru>
2024-11-28 14:01:58 +0500

    add constant.php and replace 'sqlite.db' to global constant DB_PATH * added constants.php     * added DB_PATH constant = __DIR__ . '\sqlite.db' * changed 'sqlite.db' to global constant DB_PATH * added tab_space to table creations in db_create

--- db_create.php
+++ db_create.php
@@ -1,26 +1,26 @@
 <?php
+include_once __DIR__ . "\constants.php";
 
-// Создание файла базы данных
-$db = new SQLite3('sqlite.db');
+// Подключение к файлу базы данных
+$db = new SQLite3(DB_PATH);
 
 // Включаем поддержку внешних ключей
 $db->exec('PRAGMA foreign_keys = ON');
 
 // Создание таблицы для записей
 $db->exec('CREATE TABLE IF NOT EXISTS posts (
-`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-`user_id` INTEGER, 
-`title` TEXT, 
-`body` TEXT
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+    `user_id` INTEGER, 
+    `title` TEXT, 
+    `body` TEXT
 )');
 
 // Создание таблицы для комментариев
 $db->exec('CREATE TABLE IF NOT EXISTS comments (
-`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-`name` TEXT,
-`email` TEXT,
-`body` TEXT,
-`post_id` INTEGER, 
-FOREIGN KEY(post_id) REFERENCES posts(id)
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `name` TEXT,
+    `email` TEXT,
+    `body` TEXT,
+    `post_id` INTEGER, 
+    FOREIGN KEY(post_id) REFERENCES posts(id)
 )');
-
