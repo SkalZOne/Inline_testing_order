ca4f70c32755d4d23d84315d2a95d3ff5738c2c6
SkalZ <dima.tysyahnyy@mail.ru>
2024-11-28 19:26:31 +0500

    add error alert to input * added $searchField variable = input value * added if $searchField < 3     * added <small> with warning

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+isset($_POST['searchField']) && $searchField = $_POST['searchField'];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +16,12 @@
     <form class="form" method="POST" action="index.php">
         <div class="form__input-container">
             <input type="text" placeholder="Введите текст для поиска по комментариям" name="searchField">
+            <?php
+            // Если $searchField найдена и если она меньше 3-ех символов, выводим ошибку
+            if (isset($searchField) && strlen($searchField) < 3) {
+                echo "<small class='form__input-container__alert'>В поле поиска должно быть больше 3-ех символов</small>";
+            }
+            ?>
         </div>
         <button>Найти</button>
     </form>
